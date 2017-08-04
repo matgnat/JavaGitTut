d866ff1d6b7968930b80be5bda14b38d2e00a486
Mateusz Gnat <mateusz.gnat@mobica.com>
2017-08-04 13:31:54 +0200

    Develop_improved branch

--- src/com/magn/FirstClass.java
+++ src/com/magn/FirstClass.java
@@ -10,6 +10,7 @@ public class FirstClass {
         System.out.println("Initial commit");
         System.out.println("Create dev branch");
         System.out.println("Edit directly on remote repo");
+        System.out.println("Add feature_1");
     }
 
 }
