38882bd73cdd738d83b7d95e28a624a0635379d2
Mateusz Gnat <mateusz.gnat@mobica.com>
2017-08-04 14:29:04 +0200

    3 commit f_2.1

--- src/com/magn/FirstClass.java
+++ src/com/magn/FirstClass.java
@@ -11,7 +11,7 @@ public class FirstClass {
         System.out.println("Create dev branch");
         System.out.println("Edit directly on remote repo");
         System.out.println("Add feature_1");
-        System.out.println("Add feature_2.1_I");
+        System.out.println("Add feature_2.1_I_II");
     }
 
 }
