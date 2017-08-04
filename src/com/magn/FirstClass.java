8b93f4d0424465c3a45f40d477e823dc7a9ae942
magn <mateusz.gnat@mobica.com>
2017-08-04 12:30:24 +0200

    initial commit

---
+++ src/com/magn/FirstClass.java
@@ -0,0 +1,13 @@
+package com.magn;
+
+/**
+ * Created by magn on 8/4/2017.
+ */
+public class FirstClass {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Initial commit");
+    }
+
+}
