10ab03727c15db301ae886509ee9fe7d129966c6
Mateusz Gnat <mateusz.gnat@mobica.com>
2017-08-04 12:45:42 +0200

    Initial commit on dev branch

--- src/com/magn/FirstClass.java
+++ src/com/magn/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
     public static void main(String[] args)
     {
         System.out.println("Initial commit");
+        System.out.println("Create dev branch");
     }
 
 }
